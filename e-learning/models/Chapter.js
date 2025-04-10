53c2065fa604d142887a7bf3c962289eb1f18957
IsraaBoulaares <israa.boulaares@esprit.tn>
2025-04-10 03:44:57 +0100

    front

--- e-learning/models/Chapter.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ChapterSchema = new mongoose.Schema({
-    moduleId: { type: mongoose.Schema.Types.ObjectId, ref: 'Module', required: true },
-    title: { type: String, required: true },
-    content: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Chapter', ChapterSchema);
--- e-learning/models/Module.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const ModuleSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Module', ModuleSchema);
