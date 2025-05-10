9f771396d0e18f617c4f32f8e078f0d2215413da
Ghazi saoudi <101141928+ghazy001@users.noreply.github.com>
2025-05-10 21:24:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -314,4 +314,9 @@ app.use("/scoreQuizzes", ScoreQuizRoutes);
 const server = http.createServer(app);
 server.listen(3000, () => console.log("Server running on port 3000"));
 
-module.exports = app;
+
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = { add,app };
